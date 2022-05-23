d76dc3551525cd440b726a9fa7715037f98f7d03
erwan-M <72044236+erwan-m@users.noreply.github.com>
2022-05-23 13:14:55 +0200

    create vendor

---
+++ Vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'erwan/rezenlite',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'erwan/rezenlite' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.6.0',
+            'version' => '6.6.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../Vendor/phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => 'e43bac82edc26ca04b36143a48bde1c051cfd5b1',
+            'dev_requirement' => false,
+        ),
+        'robthree/twofactorauth' => array(
+            'pretty_version' => '1.8.2',
+            'version' => '1.8.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../Vendor/robthree/twofactorauth',
+            'aliases' => array(),
+            'reference' => '65681de5a324eae05140ac58b08648a60212afc0',
+            'dev_requirement' => false,
+        ),
+    ),
+);
